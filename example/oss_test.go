2e8e64b1f27be80bd0708054c2213c2cb411892b
ksing <341532642@qq.com>
2023-11-24 23:14:48 +0800

    测试SDK

--- example/oss_test.go
+++ example/oss_test.go
@@ -2,6 +2,7 @@ package example_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
@@ -11,10 +12,17 @@ var (
 	client *oss.Client
 )
 
+var (
+	AccessKey       = os.Getenv("ALI_AK")
+	AccessKeySecret = os.Getenv("ALI_SK")
+	OssEndpoint     = os.Getenv("ALI_OSS_ENDPOINT")
+	BucketName      = os.Getenv("ALI_BUCKET_NAME")
+)
+
 // 测试oss基本功能
 func TestBucketList(t *testing.T) {
-
 	lsRes, err := client.ListBuckets()
+
 	if err != nil {
 		// HandleError(err)
 		t.Log(err)
@@ -26,7 +34,7 @@ func TestBucketList(t *testing.T) {
 }
 
 func init() {
-	c, err := oss.New("Endpoint", "AccessKeyId", "AccessKeySecret")
+	c, err := oss.New(OssEndpoint, AccessKey, AccessKeySecret)
 	if err != nil {
 		// HandleError(err)
 		panic(err)
